ca50d99a1c930ed6ea865509afca45e38875f384
Bartek <bartek@ferenji.(none)>
2012-09-02 13:06:26 -0400

    moved urls to their own entity

--- models/remote.py
+++ models/remote.py
@@ -7,20 +7,27 @@ from elixir import *
 from elixir.entity import Entity
 
 
+# used to store urls
+class Url(Entity):
+    value = Field(String(2000))
+    status = Field(String(200)) # [listed, downloaded, processed, expired]
+
 
 # represents individual posting
 class Posting(Entity):
-    url = Field(String(2000))
+    url = ManyToOne('Url')
     content = Field(Text(100000)) # raw html of the downloaded posting
 
     def __init__(self, url=None):
         self.url = url
         
     # download the body of this posting
-    def download(self, url=None):
+    def download(self, url=None, mark=True):
         if None != url:
             self.url = url
-        r = requests.get(self.url)
+        r = requests.get(self.url.value)
+        if mark and 200 == r.status_code:
+            self.url.status = 'downloaded'
         self.content = r.content
-        print '{0}: {1}'.format(self.url, r.status_code)
+        print '{0}: {1}'.format(self.url.value, r.status_code)
 
--- operations/download.py
+++ operations/download.py
@@ -1,6 +1,6 @@
 import json
 from extract import Extractor
-from models.remote import Posting
+from models.remote import Url, Posting
 from elixir import *
 
 
@@ -13,34 +13,35 @@ class Listing(object):
         fh.close()
         self.config = config
 
-    # list all the postings new since last visit
-    @property
-    def posting_urls(self):
-        next_page_url = self.config['url']
+    # retrieve postings new since last visit
+    def update(self):
+        next_page_url = Url(value=self.config['url'], status='index')
         page_limit = int(self.config['page_limit'])
         page_count = 1
         posting_urls = []
         e = Extractor(self.config['fields'])
         while None != next_page_url:
             listing = Posting(next_page_url)
-            listing.download()
+            listing.download(mark=False)
             result = e.extract(listing.content)
-            listing.delete()
             # get url of next page
-            next_page_url = result['next_page_url']
+            next_page_url = Url(value=result['next_page_url'], status='index')
             # make sure the url is absolute
-            if 'http' not in next_page_url:
-                next_page_url = self.config['url'] + next_page_url
+            if 'http' not in next_page_url.value:
+                next_page_url.value = self.config['url'] + next_page_url.value
             # get all new posting urls
             for posting_url in result['posting_url']:
-                if Posting.query.filter(Posting.url == posting_url).count() > 0:
+                if Url.query.filter(Url.value == posting_url).count() > 0:
+                    # signal break if a known url found
                     next_page_url = None
                 else:
-                    posting_urls.append(posting_url)
+                    url = Url(value=posting_url, status='listed')
+                    posting_urls.append(url)
+            session.commit()
             page_count += 1
+            # break if page limit reached
             if page_count > page_limit:
                 break
-        return posting_urls
 
 
 # encapsulates logic for downloading & storing postings
@@ -48,7 +49,8 @@ class Retriever(object):
     
     # given a listing object, retrieve all new postings
     def scrape(self, listing):
-        for url in listing.posting_urls:
+        listing.update()
+        for url in Url.query.filter(Url.status == 'listed').all():
             p = Posting(url)
             p.download()
             session.commit()
