0bae9bc93ef07f897e5f9e821036113148220a6f
Bartek Borowinski <bborowin@gmail.com>
2012-09-08 00:51:54 -0400

    fixed matches/mismatches

--- find.py
+++ find.py
@@ -7,7 +7,7 @@ import sys, json
 from time import sleep
 from elixir import *
 from operations.search import *
-
+from models.local import RentalApartment as ra
 
 metadata.bind = "mysql://obtanium:obtanium@localhost/obtanium?charset=utf8"
 setup_all()
@@ -21,13 +21,29 @@ for c in config_paths:
     fh.close()
     configs.append(config)
 
+apartments = [(r[0],0) for r in session.query(ra.id).all()]
+results = [{},{}]
 ls = LocationSearch()
 ps = PriceSearch()
 rs = RoomsSearch()
-results = {}
 ls.chain(configs[0]["location"],results)
 ps.chain(configs[0]["price"],results)
 rs.chain(configs[0]["rooms"],results)
+print
 
+ids = []
+buckets = {}
+results=results[0]
 for r in results:
-    if results[r] >= 14: print results[r], r
+    if results[r] in buckets:
+        buckets[results[r]].append(r)
+    else:
+        buckets[results[r]] = [r]
+
+apartments = ra.query.filter(ra.id.in_([r for r in results.keys()])).all()
+for a in apartments:
+    print a
+for k in buckets.keys():
+    print k, len(buckets[k])
+#[len(buckets[i]) for i in buckets.keys()]
+print 'total:', len(apartments)
--- operations/search.py
+++ operations/search.py
@@ -3,7 +3,7 @@
     search for postings containing multiple attributes
     
     each attribute has a score associated with it
-    postitive and negative matches: +score for matching attr, -score for explicit mismatch, +0 for no data on given attribute-posting pair
+    postitive and mismatches matches: +score for matching attr, -score for explicit mismatch, +0 for no data on given attribute-posting pair
 
     filter by each attribute, returning a list of (postingId, score) tuples
     compile result list by summing scores for each postingid and ordering the list descending by score
@@ -11,47 +11,67 @@
 
 from elixir import *
 from sqlalchemy.sql import between
-from models.local import *
+from models.local import RentalApartment as ra, Location
 
 
 class AttributeSearch(object):
 
     # returns a list of ids matching search parameters
-    def result(self, config, results):
+    def find(self, config):
         return []
 
     def chain(self, config, results):
-        local = [l[0] for l in self.find(config, results)]
-        for l in local:
-            if l in results: results[l] += config['score']
-            else: results[l] = config['score']
+        lists = self.find(config)
+        matches = [l[0] for l in lists[0]]
+        mismatches = [l[0] for l in lists[1]]
+        print len(matches), len(mismatches)
+        for m in matches:
+            if m not in results[1]:
+                if m in results[0]:
+                    results[0][m] += config['score']
+                else:
+                    results[0][m] = config['score']
+        for m in mismatches:
+            if m in results[1]:
+                results[1][m] -= config['score']
+            else:
+                results[1][m] = -config['score']
+            if m in results[0]:
+                del(results[0][m])
         return results
 
 
 class LocationSearch(AttributeSearch):
     accuracies = ['bus_station', 'hospital', 'intersection', 'neighborhood', 'point_of_interest', 'postal_code', 'postal_code_prefix', 'route', 'street_address', 'subpremise', 'subway_station', 'train_station', 'university']
     
-    def find(self, config, results):
+    def find(self, config):
         hits = []
         for t in config['targets']:
             latitude = float(t['center'][0])
             longitude = float(t['center'][1])
             radius = float(t['radius'])
-            locations = session.query(Location.id).filter(Location.accuracy.in_(self.accuracies)).filter(between(Location.latitude, latitude-radius, latitude+radius)).filter(between(Location.longitude, longitude-radius, longitude+radius)).all()
+            #locations = session.query(Location.id).filter(Location.accuracy.in_(self.accuracies)).filter(between(Location.latitude, latitude-radius, latitude+radius)).filter(between(Location.longitude, longitude-radius, longitude+radius)).all()
+            locations = session.query(Location.id).filter(between(Location.latitude, latitude-radius, latitude+radius)).filter(between(Location.longitude, longitude-radius, longitude+radius)).all()
             for l in locations:
                 if l[0] not in hits: hits.append(l[0])
-        return session.query(RentalApartment.id).filter(RentalApartment.location_id.in_(hits)).order_by(RentalApartment.price.desc()).all()
+        matches = session.query(ra.id).filter(ra.location_id.in_(hits)).all()
+        mismatches = session.query(ra.id).filter(~ra.location_id.in_(hits)).all()
+        return matches, mismatches
 
 
 class PriceSearch(AttributeSearch):
-    def find(self, config, results):
+    def find(self, config):
         minimum = config['min']
         maximum = config['max']
-        return session.query(RentalApartment.id).filter(between(RentalApartment.price, minimum, maximum)).order_by(RentalApartment.posted.desc()).all()
+        matches = session.query(ra.id).filter(between(ra.price, minimum, maximum)).order_by(ra.posted.desc()).all()
+        mismatches = session.query(ra.id).filter(~between(ra.price, minimum, maximum)).order_by(ra.posted.desc()).all()
+        return matches, mismatches
 
 
 class RoomsSearch(AttributeSearch):
-    def find(self, config, results):
+    def find(self, config):
         minimum = config['min']
         maximum = config['max']
-        return session.query(RentalApartment.id).filter(between(RentalApartment.rooms, minimum, maximum)).order_by(RentalApartment.posted.desc()).all()
+        matches = session.query(ra.id).filter(between(ra.rooms, minimum, maximum)).order_by(ra.posted.desc()).all()
+        mismatches = session.query(ra.id).filter(~between(ra.rooms, minimum, maximum)).order_by(ra.posted.desc()).all()
+        return matches, mismatches
